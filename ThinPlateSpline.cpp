e482ed03d13065297597d7b1b47bde9b508c2b01
Naoto Kondo <cgigcp3yqt@gmail.com>
2018-01-08 00:59:42 +0900

    clang format

--- ThinPlateSpline.cpp
+++ ThinPlateSpline.cpp
@@ -3,62 +3,59 @@
 
 ThinPlateSpline::ThinPlateSpline(const std::vector<Eigen::Vector3d> &src,
                                  const std::vector<Eigen::Vector3d> &dst)
-    : mSrcPoints(src)
-    , mDstPoints(dst)
-{}
+    : mSrcPoints(src), mDstPoints(dst) {}
 
-void ThinPlateSpline::solve()
-{
-    if (mSrcPoints.size() != mDstPoints.size())
-        return;
+void ThinPlateSpline::solve() {
+  if (mSrcPoints.size() != mDstPoints.size())
+    return;
 
-    const int num(int(mSrcPoints.size()));
-    const int rows(num + 3 + 1);
+  const int num(int(mSrcPoints.size()));
+  const int rows(num + 3 + 1);
 
-    // Create L Matrix
-    mL = Eigen::MatrixXd::Zero(rows, rows);
+  // Create L Matrix
+  mL = Eigen::MatrixXd::Zero(rows, rows);
 
-    for (int i(0); i < num; ++i) {
+  for (int i(0); i < num; ++i) {
 
-        int j(i + 1);
+    int j(i + 1);
 
-        for (; j < num; ++j)
-            mL(i, j) = mL(j, i) = radialBasis((mSrcPoints[std::size_t(i)] - mSrcPoints[std::size_t(j)]).norm());
+    for (; j < num; ++j)
+      mL(i, j) = mL(j, i) = radialBasis(
+          (mSrcPoints[std::size_t(i)] - mSrcPoints[std::size_t(j)]).norm());
 
-        mL(j, i) = mL(i, j) = 1.0;
-        ++j;
+    mL(j, i) = mL(i, j) = 1.0;
+    ++j;
 
-        for (int posElm(0); j < rows; ++posElm, ++j)
-            mL(j, i) = mL(i, j) = mSrcPoints[std::size_t(i)][posElm];
-    }
+    for (int posElm(0); j < rows; ++posElm, ++j)
+      mL(j, i) = mL(i, j) = mSrcPoints[std::size_t(i)][posElm];
+  }
 
-    // Create Y Matrix
-    Eigen::MatrixXd Y = Eigen::MatrixXd::Zero(rows, 3);
+  // Create Y Matrix
+  Eigen::MatrixXd Y = Eigen::MatrixXd::Zero(rows, 3);
 
-    for (int i(0); i < num; ++i)
-        Y.row(i) = mDstPoints[std::size_t(i)];
+  for (int i(0); i < num; ++i)
+    Y.row(i) = mDstPoints[std::size_t(i)];
 
-    // Solve L W^T = Y as W^T = L^-1 Y
-    mW = mL.colPivHouseholderQr().solve(Y);
+  // Solve L W^T = Y as W^T = L^-1 Y
+  mW = mL.colPivHouseholderQr().solve(Y);
 }
 
-Eigen::Vector3d ThinPlateSpline::interpolate(const Eigen::Vector3d &p) const
-{
-    Eigen::Vector3d res = Eigen::Vector3d::Zero();
-    int i(0);
+Eigen::Vector3d ThinPlateSpline::interpolate(const Eigen::Vector3d &p) const {
+  Eigen::Vector3d res = Eigen::Vector3d::Zero();
+  int i(0);
 
-    for (; i < mW.rows() - (3 + 1); ++i) {
+  for (; i < mW.rows() - (3 + 1); ++i) {
 
-        double rb = radialBasis((mSrcPoints[std::size_t(i)] - p).norm());
+    double rb = radialBasis((mSrcPoints[std::size_t(i)] - p).norm());
 
-        res += mW.row(i) * rb;
-    }
+    res += mW.row(i) * rb;
+  }
 
-    res += mW.row(i);
-    i++;
+  res += mW.row(i);
+  i++;
 
-    for (int j(0); j < 3; ++j, ++i)
-        res += mW.row(i) * p[j];
+  for (int j(0); j < 3; ++j, ++i)
+    res += mW.row(i) * p[j];
 
-    return res;
+  return res;
 }
--- ThinPlateSpline.hpp
+++ ThinPlateSpline.hpp
@@ -1,40 +1,46 @@
 #pragma once
 
-#include <vector>
 #include <Eigen/Core>
+#include <vector>
 
-class ThinPlateSpline
-{
+class ThinPlateSpline {
 public:
-    ThinPlateSpline() {}
-    ThinPlateSpline(const std::vector<Eigen::Vector3d> &src, const std::vector<Eigen::Vector3d> &dst);
-    ~ThinPlateSpline() {}
+  ThinPlateSpline() {}
+  ThinPlateSpline(const std::vector<Eigen::Vector3d> &src,
+                  const std::vector<Eigen::Vector3d> &dst);
+  ~ThinPlateSpline() {}
 
-    /* Solve */
-    void solve();
+  /* Solve */
+  void solve();
 
-    /* Interpolate */
-    Eigen::Vector3d interpolate(const Eigen::Vector3d &p) const;
+  /* Interpolate */
+  Eigen::Vector3d interpolate(const Eigen::Vector3d &p) const;
 
-    /* Source Points */
-    const std::vector<Eigen::Vector3d> &srcPoints() const { return mSrcPoints; }
+  /* Source Points */
+  const std::vector<Eigen::Vector3d> &srcPoints() const { return mSrcPoints; }
 
-    /* Set Source Points */
-    void setSrcPoints(const std::vector<Eigen::Vector3d> &points) { mSrcPoints = points; }
+  /* Set Source Points */
+  void setSrcPoints(const std::vector<Eigen::Vector3d> &points) {
+    mSrcPoints = points;
+  }
 
-    /* Destination Points */
-    const std::vector<Eigen::Vector3d> &dstPoints() const { return mDstPoints; }
+  /* Destination Points */
+  const std::vector<Eigen::Vector3d> &dstPoints() const { return mDstPoints; }
 
-    /* Set Destination Points */
-    void setDstPoints(const std::vector<Eigen::Vector3d> &points) { mDstPoints = points; }
+  /* Set Destination Points */
+  void setDstPoints(const std::vector<Eigen::Vector3d> &points) {
+    mDstPoints = points;
+  }
 
 protected:
-    /* Radial Basis Function */
-    static inline double radialBasis(double r) { return r == 0.0 ? r : r * r * log(r); }
+  /* Radial Basis Function */
+  static inline double radialBasis(double r) {
+    return r == 0.0 ? r : r * r * log(r);
+  }
 
-    /* Data */
-    std::vector<Eigen::Vector3d> mSrcPoints;
-    std::vector<Eigen::Vector3d> mDstPoints;
-    Eigen::MatrixXd mW;
-    Eigen::MatrixXd mL;
+  /* Data */
+  std::vector<Eigen::Vector3d> mSrcPoints;
+  std::vector<Eigen::Vector3d> mDstPoints;
+  Eigen::MatrixXd mW;
+  Eigen::MatrixXd mL;
 };
